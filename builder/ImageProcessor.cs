130070340609b6dabfff2a28f19fdeb0ad9cb2a8
shawnhar@microsoft.com <shawnhar@microsoft.com>
2021-05-16 21:13:54 -0700

    Apply 1.08 distance normalization factor to total length of all trails

--- builder/ImageProcessor.cs
+++ builder/ImageProcessor.cs
@@ -14,7 +14,7 @@ namespace builder
     class ImageProcessor
     {
         // Manually measured using CalTopo. Does not double count any overlaps or out-and-back.
-        const float totalLengthOfAllTrails = 286;
+        const float totalLengthOfAllTrails = 309;
 
         public CanvasDevice Device { get; private set; }
         public CanvasBitmap MasterMap { get; private set; }
