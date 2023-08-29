3ee4b04308ebbc7c0eac7c529e434cf3be7d0f8d
Shawn Hargreaves <shawnhargreaves@hotmail.com>
2023-08-28 20:29:09 -0700

    Add Brown Peak and Green Park

--- builder/ImageProcessor.cs
+++ builder/ImageProcessor.cs
@@ -15,7 +15,7 @@ namespace builder
     class ImageProcessor
     {
         // Manually measured using CalTopo. Does not double count any overlaps or out-and-back.
-        const float totalLengthOfAllTrails = 355;
+        const float totalLengthOfAllTrails = 357;
 
         public CanvasDevice Device { get; private set; }
         public CanvasBitmap MasterMap { get; private set; }
