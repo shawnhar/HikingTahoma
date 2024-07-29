3adc1421127955fd2a2dac795375700fa7a45e41
Shawn Hargreaves <shawnhargreaves@hotmail.com>
2024-07-28 20:03:26 -0700

    Add Mount Ararat

--- builder/ImageProcessor.cs
+++ builder/ImageProcessor.cs
@@ -15,7 +15,7 @@ namespace builder
     class ImageProcessor
     {
         // Manually measured using CalTopo. Does not double count any overlaps or out-and-back.
-        const float totalLengthOfAllTrails = 366;
+        const float totalLengthOfAllTrails = 367;
 
         public CanvasDevice Device { get; private set; }
         public CanvasBitmap MasterMap { get; private set; }
